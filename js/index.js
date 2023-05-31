0b89b600dc999251670506bdbb4ed87f605b0a83
RonaldValverde44 <rvalverde@unitru.edu.pe>
2023-05-30 23:13:37 -0500

    feat: inicializating with java script

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// alert('Bienvenido a X');
+// const age = prompt('Digite su edad');
+// if (age >= 18) {
+//   alert('Bienvenido');
+// } else {
+//   window.location.href = 'https://www.policia.gob.pe';
+// }
+for (let index = 0; index < 12; index++) {
+  console.log(12 * (index + 1));
+}
